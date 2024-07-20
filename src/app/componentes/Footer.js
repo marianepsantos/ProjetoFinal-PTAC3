7bfd2d6e77ae3c38412487b0b51378dd518f2772
marianepsantos <marianepsantos15@gmail.com>
2024-07-20 15:10:56 -0300

    Commit
    Implementando a página Footer.js- #Servidor

---
+++ src/app/componentes/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Styles from "./footer.module.css"
+import Image from "next/image";
+
+export default function Footer(){
+    return(
+        <footer className={styles.footer}>
+
+            <Image className={styles.imgfooter}
+            width={100}
+            height={100}
+            src = {"https://wepink.vtexassets.com/assets/vtex/assets-builder/wepink.store-theme/3.0.12/svg/logo-primary___5bd0e7fa6451ba181395889123dfe00a.svg"}></Image>
+        </footer>
+    )}
